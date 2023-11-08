61e0bfd177c8948a640cf751c4e39ca08183b82c
MUHAMMAD ALI <malirashid1994@gmail.com>
2023-11-09 00:17:43 +0500

    Update donor.py

--- apps/donor.py
+++ apps/donor.py
@@ -47,7 +47,7 @@ def app():
     if st.button('Find NGO'):
         features_lst = list(features.values())
         input_dict = np.array([features_lst])*1.0
-        st,write(input_dict)
+        st.write(input_dict)
         predictions = model.predict(input_dict,verbose = 0)
         cls=np.argmax(predictions[0])
         prediction=sweden_food_banks_dict[cls]
