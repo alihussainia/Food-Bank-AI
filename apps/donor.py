6787ef33d58b5052c2d17d13d42116a980f13184
MUHAMMAD ALI <malirashid1994@gmail.com>
2023-11-23 09:15:48 +0500

    Fixing indentation issues

--- apps/donor.py
+++ apps/donor.py
@@ -76,36 +76,39 @@ def app():
                 save_to_trubrics=True,
                 key=st.session_state.count,
                 align="center")
-            if user_feedback1:
+            
+            st.session_state.count += 1
+            st.write("How do you feel about the App idea?")
+            user_feedback2 = collector.st_feedback(
+                component="IdeaResponse",
+                feedback_type="faces",
+                model=model,
+                metadata={"input_features":features, "predicted_class": prediction},
+                save_to_trubrics=True,
+                key=st.session_state.count,
+                align="center")
+		            
+            if user_feedback2:
                 st.session_state.count += 1
-                st.write("How do you feel about the App idea?")
-                user_feedback2 = collector.st_feedback(
-                    component="IdeaResponse",
-                    feedback_type="faces",
+                st.write("[Optional] Provide any additional feedback about the App")
+                user_feedback3 = collector.st_feedback(
+                    component="FeedbackResponse",
+                    feedback_type="textbox",
+                    textbox_type="text-input",
+                    open_feedback_label="",
                     model=model,
                     metadata={"input_features":features, "predicted_class": prediction},
-                    save_to_trubrics=True,
                     key=st.session_state.count,
-                    align="center")
-		            
-                if user_feedback2:
-                    st.session_state.count += 1
-                    st.write("[Optional] Provide any additional feedback about the App")
-                    user_feedback3 = collector.st_feedback(
-                        component="FeedbackResponse",
-                        feedback_type="textbox",
-                        textbox_type="text-input",
-                        open_feedback_label="",
-                        model=model,
-                        metadata={"input_features":features, "predicted_class": prediction},
-                        key=st.session_state.count,
-                        save_to_trubrics=True,
-                        align="center") 
+                    save_to_trubrics=True,
+                    align="center") 
             
             submitted1 = st.form_submit_button('Submit Feedback')
         
         if submitted1:
             st.toast("Thank You for Using Food Bank!")
+            st.write(user_feedback1)
+            st.write(user_feedback2)
+            st.write(user_feedback3)
             # st.session_state.feedback_key = 0
             #st.experimental_rerun()
 
