bcb4d1bf8b769996dee0a9849e48b5e3cdfc4d41
MUHAMMAD ALI <malirashid1994@gmail.com>
2023-11-23 08:38:57 +0500

    Update donor.py

--- apps/donor.py
+++ apps/donor.py
@@ -67,44 +67,45 @@ def app():
         st.header('Feedback Form', divider='rainbow')
 
         st.write('Please provide your feedback below :point_down:')
-        # if st.session_state.feedback_key==0:
         
-        with st.form(key='feedback_key'):
-	    st.write("Do you support Dark Theme for this App?")
-	    user_feedback1 = collector.st_feedback(
-	    component="DarkUIResponse",
-	    feedback_type="thumbs",
-	    model=model,
-	    metadata={"input_features":features, "predicted_class": prediction},
-	    save_to_trubrics=True,
-	    key=st.session_state.count,
-	    align="center")
-	    if user_feedback1:
-		st.session_state.count += 1
-		st.write("How do you feel about the App idea?")
-		user_feedback2 = collector.st_feedback(
-		component="IdeaResponse",
-		feedback_type="faces",
-		model=model,
-		metadata={"input_features":features, "predicted_class": prediction},
-		save_to_trubrics=True,
-		key=st.session_state.count,
-		align="center")
-		if user_feedback2:
-		    st.session_state.count += 1
-		    st.write("[Optional] Provide any additional feedback about the App")
-		    user_feedback3 = collector.st_feedback(
-		    component="FeedbackResponse",
-		    feedback_type="textbox",
-		    textbox_type="text-input",
-		    open_feedback_label="",
-		    model=model,
-		    metadata={"input_features":features, "predicted_class": prediction},
-		    key=st.session_state.count,
-		    save_to_trubrics=True,
-		    align="center") 
+        with st.form(key='count'):
+            st.write("Do you support Dark Theme for this App?")
+            user_feedback1 = collector.st_feedback(
+                component="DarkUIResponse",
+                feedback_type="thumbs",
+                model=model,
+                metadata={"input_features":features, "predicted_class": prediction},
+                save_to_trubrics=True,
+                key=st.session_state.count,
+                align="center")
+		
+	        if user_feedback1:
+		        st.session_state.count += 1
+		        st.write("How do you feel about the App idea?")
+		        user_feedback2 = collector.st_feedback(
+                    component="IdeaResponse",
+            		feedback_type="faces",
+            		model=model,
+            		metadata={"input_features":features, "predicted_class": prediction},
+            		save_to_trubrics=True,
+            		key=st.session_state.count,
+            		align="center")
+		            
+                    if user_feedback2:
+            		    st.session_state.count += 1
+            		    st.write("[Optional] Provide any additional feedback about the App")
+		                user_feedback3 = collector.st_feedback(
+                            component="FeedbackResponse",
+                		    feedback_type="textbox",
+                		    textbox_type="text-input",
+                		    open_feedback_label="",
+                		    model=model,
+                		    metadata={"input_features":features, "predicted_class": prediction},
+                		    key=st.session_state.count,
+                		    save_to_trubrics=True,
+                		    align="center") 
             
-        	submitted1 = st.form_submit_button('Submit Feedback')
+            submitted1 = st.form_submit_button('Submit Feedback')
         
         if submitted1:
             st.toast("Thank You for Using Food Bank!")
